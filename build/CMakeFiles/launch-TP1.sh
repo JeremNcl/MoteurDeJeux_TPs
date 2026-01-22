1e3917d86ac7edab987c2d6583016e1a2740e767
JeremNcl <jerem57ncl@gmail.com>
2026-01-22 10:16:30 +0100

    first commit

---
+++ build/CMakeFiles/launch-TP1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+bindir=$(pwd)
+cd /home/e20250002139/Documents/Moteur_de_jeux/TP1/HAI819I_TP1/TP1_code/TP1/
+export 
+
+if test "x$1" = "x--debugger"; then
+	shift
+	if test "xYES" = "xYES"; then
+		echo "r  " > $bindir/gdbscript
+		echo "bt" >> $bindir/gdbscript
+		/usr/bin/gdb -batch -command=$bindir/gdbscript --return-child-result /home/e20250002139/Documents/Moteur_de_jeux/TP1/HAI819I_TP1/TP1_code/build/TP1 
+	else
+		"/home/e20250002139/Documents/Moteur_de_jeux/TP1/HAI819I_TP1/TP1_code/build/TP1"  
+	fi
+else
+	"/home/e20250002139/Documents/Moteur_de_jeux/TP1/HAI819I_TP1/TP1_code/build/TP1"  
+fi
